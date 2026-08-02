f1e7e20c9a0783d2e7690f029899409a48def3af
Njabulo <njabulomadlala011@gmail.com>
2026-08-02 16:03:58 +0200

    quiz logic-fetch data

--- quiz.js
+++ quiz.js
@@ -1,28 +1,48 @@
-import {collection, getDocs, query, where, orderBy, limit} from "firebase/firestore";
-import {db} from "./firebaseConfig.js";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+} from "firebase/firestore";
+import { db } from "./firebaseConfig.js";
+import express from "express";
 
-const animalData = []
+const animalData = [];
 
-const getAnimalByName = async () => {
-    const randomGenerator = Math.floor(Math.random() * 60);
-    console.log("Random number generated:", randomGenerator);
-    try{
-        const q = query(collection(db,"animals"));
-        const querySnapshot = await getDocs(q);
-        if (querySnapshot) {
-          querySnapshot.forEach((doc) => {
-            console.log(doc.id, " => ", doc.data());
-            animalData.push(doc.data());
-        });
-        }
-        
-    } catch (error) {
-        console.error("Error getting animal:", error);
+async function getAnimalByName() {
+  try {
+    const q = query(collection(db, "animals"));
+    const querySnapshot = await getDocs(q);
+    if (querySnapshot) {
+      const animals = querySnapshot.docs.map((doc) => doc.data());
+      animalData.push(...animals);
+      
     }
+  } catch (error) {
+    console.error("Error getting animal:", error);
+  }
+}
 
-};
+async function getRandomAnimal(animalData) {
+  const answerGenerator = Math.floor(Math.random() * animalData.length);
+  
+  const quizAnswer = animalData[answerGenerator];
 
+  const randomNames = [];
+  while (randomNames.length < 4) {
+    const randomIndex = Math.floor(Math.random() * animalData.length);
+    const responseNames = animalData[randomIndex].Name;
+    randomNames.push(responseNames);
+  }
+  console.log(quizAnswer);
+  console.log(randomNames);
+}
 
+async function getQuiz() {
+  await getAnimalByName();
+  await getRandomAnimal(animalData);
+}
 
-
-getAnimalByName()
+getQuiz().catch(console.error);
